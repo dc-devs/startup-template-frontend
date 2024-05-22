4376901db137cd775f88ff425dc8b77a50df45a1
David Christian <david.w.christian@gmail.com>
2024-05-22 08:05:13 -0700

    Tanstack + Tailwind installed

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import { defineConfig } from 'vite';
+import tailwindcss from 'tailwindcss';
+import react from '@vitejs/plugin-react';
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    // ...,
-    TanStackRouterVite(),
-  ],
-})
+	plugins: [react(), TanStackRouterVite()],
+	css: {
+		postcss: {
+			plugins: [tailwindcss()],
+		},
+	},
+});
