74b6cfe465a4c41e918baf7352122c7895dc2616
David Christian <david.w.christian@gmail.com>
2024-06-03 08:56:10 -0700

    Basic Tanstack Form implemeted

--- src/app/SignIn.tsx
+++ src/app/SignIn.tsx
@@ -3,8 +3,32 @@ import { Button } from '../components/Button';
 import { TextField } from '../components/Fields';
 import { Logo } from '../components/Logo';
 import { SlimLayout } from '../components/SlimLayout';
+import { useForm } from '@tanstack/react-form';
+import type { FieldApi } from '@tanstack/react-form';
+
+const FieldInfo = ({ field }: { field: FieldApi<any, any, any, any> }) => {
+	return (
+		<>
+			{field.state.meta.touchedErrors ? (
+				<em>{field.state.meta.touchedErrors}</em>
+			) : null}
+			{field.state.meta.isValidating ? 'Validating...' : null}
+		</>
+	);
+};
 
 export const SignIn = () => {
+	const form = useForm({
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+		onSubmit: async ({ value }) => {
+			// Do something with form data
+			console.log('[Form]', value);
+		},
+	});
+
 	return (
 		<SlimLayout>
 			<div className="flex justify-center">
@@ -25,33 +49,101 @@ export const SignIn = () => {
 				</Link>{' '}
 				for a free trial.
 			</p>
-			<form action="#" className="mt-10 grid grid-cols-1 gap-y-8">
-				<TextField
-					label="Email address"
-					name="email"
-					type="email"
-					autoComplete="email"
-					required
-				/>
-				<TextField
-					label="Password"
-					name="password"
-					type="password"
-					autoComplete="current-password"
-					required
-				/>
+			<form
+				className="mt-10 grid grid-cols-1 gap-y-8"
+				onSubmit={(e) => {
+					e.preventDefault();
+					e.stopPropagation();
+					form.handleSubmit();
+				}}
+			>
 				<div>
-					<Button
-						type="submit"
-						variant="solid"
-						color="blue"
-						className="w-full"
-					>
-						<span>
-							Sign in <span aria-hidden="true">&rarr;</span>
-						</span>
-					</Button>
+					{/* A type-safe field component*/}
+					<form.Field
+						name="email"
+						validators={{
+							onChange: ({ value }) =>
+								!value
+									? 'An email is required'
+									: value.length < 3
+										? 'email must be at least 3 characters'
+										: undefined,
+							onChangeAsyncDebounceMs: 500,
+							onChangeAsync: async ({ value }) => {
+								await new Promise((resolve) =>
+									setTimeout(resolve, 1000),
+								);
+								return (
+									value.includes('error') &&
+									'No "error" allowed in email'
+								);
+							},
+						}}
+						children={(field) => {
+							// Avoid hasty abstractions. Render props are great!
+							return (
+								<>
+									<TextField
+										label={field.name}
+										name={field.name}
+										type={field.name}
+										autoComplete={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) =>
+											field.handleChange(e.target.value)
+										}
+										required
+									/>
+									<FieldInfo field={field} />
+								</>
+							);
+						}}
+					/>
 				</div>
+				<div>
+					<form.Field
+						name="password"
+						children={(field) => (
+							<>
+								<TextField
+									label={field.name}
+									name={field.name}
+									type={field.name}
+									autoComplete={field.name}
+									value={field.state.value}
+									onBlur={field.handleBlur}
+									onChange={(e) =>
+										field.handleChange(e.target.value)
+									}
+									required
+								/>
+							</>
+						)}
+					/>
+				</div>
+				<form.Subscribe
+					selector={(state) => [state.canSubmit, state.isSubmitting]}
+					children={([canSubmit, isSubmitting]) => (
+						<div>
+							{isSubmitting ? (
+								'...'
+							) : (
+								<Button
+									type="submit"
+									variant="solid"
+									color="blue"
+									className="w-full"
+								>
+									<span>
+										Sign in{' '}
+										<span aria-hidden="true">&rarr;</span>
+									</span>
+								</Button>
+							)}
+						</div>
+					)}
+				/>
 			</form>
 		</SlimLayout>
 	);
